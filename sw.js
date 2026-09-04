4b7a4fcc1c207e820acb1c84de38cd2947975d1c
nonotao10015-creator <nonotao10015@gmail.com>
2026-09-05 00:19:33 +0800

    upload ./sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+var CACHE = "myshsmu-timetable-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./data/schedule.json",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (key) { return key !== CACHE; })
+          .map(function (key) { return caches.delete(key); })
+      );
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(function (hit) {
+      return hit || fetch(event.request).then(function (resp) {
+        if (resp.ok && new URL(event.request.url).origin === location.origin) {
+          var copy = resp.clone();
+          caches.open(CACHE).then(function (cache) { cache.put(event.request, copy); });
+        }
+        return resp;
+      });
+    })
+  );
+});
